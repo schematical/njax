7aa67e0fa7a3a9410e4ec4260ae43a2a7041d3df
Matt Lea <mlea@schematical.com>
2014-08-24 19:05:49 -0500

    api auth stuff

--- public/js/directives.js
+++ public/js/directives.js
@@ -27,4 +27,33 @@ angular.module('njax.directives', [])
             }*/
 
         };
-    }]);
+    }]).directive('njaxEditable', [ function() {
+
+        return {
+            replace:true,
+            scope:{
+
+
+                //
+            },
+            templateUrl: '/njax/templates/directives/njaxEditable.html',
+            link:function(scope, element, attrs) {
+                //element.val('test');
+                /*scope.popover = {
+                    "title": "Title",
+                    "content": "Tacos<br />This is a multiline message!",
+                    "html":true,
+                    "trigger":"hover"
+                };*/
+               /* var target = angular.element(document.querySelector( '#' + attrs.njaxNamespace));
+                target.on('mouseover', function(event) {
+                    var namespace = target.val()
+                    namespace = namespace.toLowerCase();
+                    namespace = namespace.replace(/[^\w\s]/gi, '');
+                    namespace = namespace.replace(/ /g,"_");
+                    element.val(namespace);
+
+                });*/
+            }
+        };
+    }]);;

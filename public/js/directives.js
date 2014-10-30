03c1877a457cf7128a25f266f4542d1e4a0b13e7
jeffersoncarpenter <jeffersoncarpenter2@gmail.com>
2014-10-30 20:20:31 +0000

    factor out a service for creating namespace names use it in one place

--- public/js/directives.js
+++ public/js/directives.js
@@ -1,5 +1,5 @@
 
-angular.module('njax.directives', [])
+angular.module('njax.directives', ['njax.services'])
     .directive('njaxIframe', [ function() {
         return {
             replace:true,
@@ -20,7 +20,7 @@ angular.module('njax.directives', [])
 
         };
     }])
-    .directive('njaxNamespace', [ function() {
+    .directive('njaxNamespace', ['getNamespace', function(getNamespace) {
 
         return {
             replace:true,
@@ -32,10 +32,7 @@ angular.module('njax.directives', [])
                 var target = angular.element(document.querySelector( '#' + attrs.njaxNamespace));
 				var ngModel = element.attr('ng-model');
 				var updateFromTarget = function(event) {
-					var namespace = target.val()
-					namespace = namespace.toLowerCase();
-					namespace = namespace.replace(/[^\w\s]/gi, '');
-					namespace = namespace.replace(/ /g,"_");
+					var namespace = getNamespace(target.val());
 					element.val(namespace);
 
 					if(ngModel){

279afaf624ced27e77d9d2e61296727904eeae6c
Matt Lea <mlea@schematical.com>
2015-09-04 10:34:37 -0500

    frontend stuff

--- public/js/builder.js
+++ public/js/builder.js
@@ -2,7 +2,7 @@
  This will build the assets from njax_config.js into working angular files
  */
 //TODO: Move this to NJax-UTIL in Bower
-var njaxServices = angular.module('njax', []);
+var njaxServices = angular.module('njax', ['ngTable']);
 njaxServices.factory('NJaxSocket', ['$q', 'NJaxBootstrap', function($q, NJaxBootstrap){
 	var socket = io('http://localhost:3030');
 
@@ -21,6 +21,7 @@ njaxServices.factory('NJaxSocket', ['$q', 'NJaxBootstrap', function($q, NJaxBoot
 			});
 		},
 		$save:function(model_name, model_data){
+
 			return NJaxSocket.$request('$save', {
 				model:model_name,
 				data:model_data
@@ -29,9 +30,10 @@ njaxServices.factory('NJaxSocket', ['$q', 'NJaxBootstrap', function($q, NJaxBoot
 		$request:function(event, data){
 			var request_id = new Date().getTime() + '-' + Math.floor(Math.random() * 1000);
 			var deferred = $q.defer();
+			data._event = event;
 			NJaxSocket._requests[request_id] = deferred;
 			data._request_id = request_id;
-			socket.emit('$query', data);
+			socket.emit(event, data);
 			return deferred.promise;
 		},
 		onResponse:function(data){
@@ -79,12 +81,31 @@ window.NJax.Builder = {
 		njaxServices.factory(
 			model.capitalName,
 			[
+				'$q',
 				'NJaxSocket',
-				function(NJaxSocket){
+				function($q, NJaxSocket){
 				var njaxResource = (function () {
 					return function (data) {
 						this._model = model;
-						this.data = data;
+						this.data = data || {};
+						var _this = this;
+						for(var key in model.fields){
+							(function(_key) {
+								Object.defineProperty(
+									_this,
+									_key,
+									{
+										set: function (val) {
+											return _this.data[_key] = val;
+										},
+										get: function () {
+											return _this.data[_key];
+										}
+									}
+								);
+							})(key);
+
+						}
 						return this;
 					}
 				})();
@@ -93,7 +114,14 @@ window.NJax.Builder = {
 					return NJaxSocket.$query(model.capitalName, query, 0);
 				}
 				njaxResource.prototype.$save = function () {
-					return NJaxSocket.$save(model.capitalName, this.data);
+					var _this = this;
+					var deferred = $q.defer();
+
+					NJaxSocket.$save(model.capitalName, this.data).then(function(data){
+						_this.data = data.response;
+						deferred.resolve(this);
+					})
+					return deferred.promise;
 				}
 				njaxResource.prototype.$archive = function () {
 
@@ -116,24 +144,81 @@ window.NJax.Builder = {
 	buildDirective:function(model){
 
 		var directiveName = model.name + 'List';
-		console.log(directiveName);
+		/**
+		 * Here is the table code
+		 */
 		njaxServices.directive.apply(
 			null, [
 				directiveName,
-				function() {
-					return {
-						replace: true,
-						scope: {
-							//application: '=njaxApplicationWidget'
-						},
-						//templateUrl: '/templates/directives/adamMap.html',
+				[
+					'ngTableParams',
+					function(ngTableParams) {
+						return {
+							replace: true,
+							scope: {
+								collection: '=collection'
+							},
+							templateUrl: '/templates/model/' + model.name + '/_table.html',
 
-						template: '<div>Template A</div>',
-						link: function (scope, element, attrs) {
-
+							//template: '<div>Template A</div>',
+							link: function (scope, element, attrs) {
+								if(!scope.collection){
+									scope.collection = [];
+								}
+								scope.tableParams = new ngTableParams(
+									{
+										page: 1,            // show first page
+										count: 10           // count per page
+									},
+									{
+										total: scope.collection.length, // length of data
+										getData: function ($defer, params) {
+											$defer.resolve(
+												scope.collection.slice((params.page() - 1) * params.count(), params.page() * params.count())
+											);
+										}
+									}
+								)
+							}
 						}
 					}
-				}
+				]
+			]
+		);
+
+
+		/**
+		 * Here is the edit form code
+		 */
+		var scope = {};
+		scope[model.name] = '=' + model.name;
+		njaxServices.directive.apply(
+			null, [
+				model.name + 'EditForm',
+				[
+					model.capitalName,
+					function(Model) {
+						return {
+							replace: true,
+							scope: scope,
+							templateUrl: '/templates/model/' + model.name + '/_edit.html',
+
+							//template: '<div>Template A</div>',
+							link: function (scope, element, attrs) {
+								if(!scope[model.name]){
+									scope[model.name] = new Model();
+								}
+								scope.save = function($event){
+									$event.preventDefault();
+									console.log("Save!")
+									scope[model.name].$save().then(function(){
+										console.log("Saved!")
+									})
+								}
+							}
+						}
+					}
+				]
 			]
 		);
 	}
